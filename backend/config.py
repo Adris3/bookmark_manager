bff2b995fa85aabd6f3759485c3765dbdce15f33
Advait Risbud <advaitris@example.com>
2026-06-15 08:38:59 -0400

    Set up the database in config.py

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,11 @@ from flask_cors import CORS
 
 app = Flask(__name__) # Initializes Flask application
 
+CORS(app) # Now we can send Cross Origin Requests between frontend (bookmarker) and backend
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db" # Specified location of local sqlite database
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app) # Create database instance that gives access to the database
+
+
