cfded74d1a63de4a3c30a2964fa7b24b6a622dd8
Advait Risbud <advaitris@example.com>
2026-06-15 10:02:22 -0400

    created fetchBookmarks function

--- bookmarker/src/App.jsx
+++ bookmarker/src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  const [bookmarks, setBookmarks] = useState([])
+
+  
+
+  const fetchBookmarks = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json()
+    setBookmarks(data.contacts)
+    console.log(data.contacts)
+  }
 
   return (
     <>
