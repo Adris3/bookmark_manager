5c98034d7881ec837fdbe64f358aa789f8df66a7
Advait Risbud <advaitris@example.com>
2026-06-15 10:55:21 -0400

    Finished code for Bookmark Form

--- bookmarker/src/BookmarkForm.jsx
+++ bookmarker/src/BookmarkForm.jsx
@@ -2,29 +2,55 @@ import { useState } from "react";
 
 const BookmarkForm = () => {
 
-    const [name, setName] = useState("")
-    const [link, setLink] = useState("")
+    const [name, setName] = useState("");
+    const [link, setLink] = useState("");
 
-    return <form>
-        <div>
-            <label htmlFor="name">Name:</label>
-            <input 
-                type="text"
-                id="name" 
-                value={name} 
-                onChange={(e) => setName(e.target.value)}
-            />
-        </div>
-        <div>
-            <label htmlFor="link">Link:</label>
-            <input 
-                type="text"
-                id="link" 
-                value={link} 
-                onChange={(e) => setLink(e.target.value)}
-            />
-        </div>
-        <button type="submit">Create Bookmark</button>
-    </form>
+    const onSubmit = async (e) => {
+        e.preventDefault()
 
-}
+        const data = {
+            name, 
+            link
+        }
+        const url = "http://127.0.0.1/create/contact"
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch(url, options)
+        if (response.status !== 201 && response.status !== 200){
+            const data = await response.json()
+            alert(data.message)
+        } else {
+            //success
+        }
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input 
+                    type="text"
+                    id="name" 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="link">Link:</label>
+                <input 
+                    type="text"
+                    id="link" 
+                    value={link} 
+                    onChange={(e) => setLink(e.target.value)}
+                />
+            </div>
+            <button type="submit">Create Bookmark</button>
+        </form>
+    );
+};
+export default BookmarkForm
