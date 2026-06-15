e774e8098082001539de6c7a050a02d58855cabf
Advait Risbud <advaitris@example.com>
2026-06-15 08:48:00 -0400

    Created a to_json function to convert all the data to a dictionairy, then to JSON.

--- backend/models.py
+++ backend/models.py
@@ -6,3 +6,9 @@ class Bookmark(db.Model):
     link = db.Column(db.String(120), unique = True, nullable = False)
 
     def to_json(self):
+        # converts all the bookmark details into a python dictionairy, then to JSON
+        return {
+            "id": self.id, 
+            "name": self.name,
+            "link": self.link
+        }
