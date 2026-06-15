d3b37065c028aaba68e924fbb50667a01b441cf0
Advait Risbud <advaitris@example.com>
2026-06-15 10:11:16 -0400

    Added useEffect()

--- bookmarker/src/App.jsx
+++ bookmarker/src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   const [bookmarks, setBookmarks] = useState([])
 
-  
+  useEffect(() => {
+    fetchBookmarks()
+  }, [])
 
   const fetchBookmarks = async () => {
     const response = await fetch("http://127.0.0.1:5000/contacts")
