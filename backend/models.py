5ac37ca43eaba60a6105c13be3a4ee9033e7c6c6
Advait Risbud <advaitris@example.com>
2026-06-15 08:45:44 -0400

    Created Bookmark class that contains details about bookmarks.

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,8 @@
 from config import db
+
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique = False, nullable = False)
+    link = db.Column(db.String(120), unique = True, nullable = False)
+
+    def to_json(self):
