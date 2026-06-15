98106b21e76d347b6a0ea0a90c2a0ff87259a7e0
Advait Risbud <advaitris@example.com>
2026-06-15 09:04:49 -0400

    Made code to create the defined models in the database

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,9 @@ from flask import request, jsonify
 from config import app, db
 from models import Bookmark
 
+if __name__ == "__main__":
+    # Creates all the defined models into the database upon start
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
